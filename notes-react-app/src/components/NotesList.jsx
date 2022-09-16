a17a4ef071bcfe3f8c49bd628bf8a8bf58ec2fde
Jonathan Chan <jonchan3990@gmail.com>
2022-09-15 22:08:35 -0700

    Pushed down addNote handler to NotesList component

--- notes-react-app/src/components/NotesList.jsx
+++ notes-react-app/src/components/NotesList.jsx
@@ -1,14 +1,16 @@
 import { React } from 'react';
 
 import './NotesList.css';
-import Note from './Note';
+import Note from './Note.jsx';
+import AddNote from './AddNote.jsx';
 
-const NotesList = ({ notes }) => {
+const NotesList = ({ notes, addNote }) => {
   return (
     <div className="notes-list">
       {notes.map(note => (
         <Note key={note.id} text={note.text} createDate={note.createDate} />
       ))}
+      <AddNote addNote={addNote} />
     </div>
   )
 }
