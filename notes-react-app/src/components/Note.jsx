28043b58d16183cf0a77a1a8f0a7ed4d15868d97
Jonathan Chan <jonchan3990@gmail.com>
2022-09-15 17:52:10 -0700

    Added test content to Note component

--- notes-react-app/src/components/Note.jsx
+++ notes-react-app/src/components/Note.jsx
@@ -1,9 +1,18 @@
 import { React } from 'react';
+import { MdDeleteForever } from 'react-icons/md';
+
+import './Note.css';
 
 const Note = () => {
   return (
-    <div className='note-container'>
-
+    <div className='note'>
+      <span>Hello! This is our first note! Hurray!</span>
+      <div className='note-footer'>
+        <small>09/15/2022</small>
+        <MdDeleteForever className='delete-icon' size='1.3em' />
+      </div>
     </div>
   )
 }
+
+export default Note;
